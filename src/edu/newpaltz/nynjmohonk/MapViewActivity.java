a609de810b615875d2cc639724e2b3a28ce8a7c8
conor.branagan@gmail.com <conor.branagan@gmail.com@b6079bb8-4db0-9ca3-396d-66b1b36275a7>
2011-02-16 16:42:45 +0000

    added touch moving in onTouch method. also has initial scaling for multitouch - although not really working

--- src/edu/newpaltz/nynjmohonk/MapViewActivity.java
+++ src/edu/newpaltz/nynjmohonk/MapViewActivity.java
@@ -3,6 +3,7 @@ package edu.newpaltz.nynjmohonk;
 import android.app.Activity;
 import android.graphics.Matrix;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -12,6 +13,7 @@ import android.widget.ImageView.ScaleType;
 public class MapViewActivity extends Activity {
 	LinearLayout mLinearLayout;
 	ImageView i;
+	float lastX = -1, lastY = -1;
 	
 	/** Called when the activity is first created. */
     @Override
@@ -27,11 +29,37 @@ public class MapViewActivity extends Activity {
     }
     
     public boolean onTouchEvent(MotionEvent e) {
-        Matrix m = i.getImageMatrix();
-        m.postTranslate(3, 3);
-        i.setImageMatrix(m);
-        i.setScaleType(ScaleType.MATRIX);
-        //i.invalidate();
+    	int p = e.getPointerCount();
+    	if(lastX == -1 && lastY == -1) {
+    		lastX = e.getX();
+    		lastY = e.getY();
+    		return true;
+    	}
+    	
+    	float dx = e.getX() - lastX;
+    	float dy = e.getY() - lastY;
+    	Log.d("NYNJ Trails", "Moving " + dx + ", " + dy);
+    	switch(e.getAction()) {
+    		case MotionEvent.ACTION_MOVE:
+    			if(p == 1) {
+	    	        Matrix m = i.getImageMatrix();
+	    	        m.postTranslate(dx, dy);
+	    	        i.setImageMatrix(m);
+	    	        i.setScaleType(ScaleType.MATRIX);
+	    	        i.invalidate();
+    			} else {
+    				// Multi-touch
+	    	        Matrix m = i.getImageMatrix();
+	    	        m.postScale((float).9, (float).9);
+	    	        i.setImageMatrix(m);
+	    	        i.setScaleType(ScaleType.MATRIX);
+	    	        i.invalidate();
+	    	    }
+    	       break;
+	    	}
+    	lastX = e.getX();
+    	lastY = e.getY();
+
         return true;
     }
 }
