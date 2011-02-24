769da0d279f4b2cf9a8402876bfbd8c9483863f4
deviant205@gmail.com <deviant205@gmail.com@b6079bb8-4db0-9ca3-396d-66b1b36275a7>
2011-02-24 02:12:01 +0000

    Added a view that controls zoom, translate, etc; began a main menu.

--- trunk/gen/edu/newpaltz/nynjmohonk/R.java
+++ trunk/gen/edu/newpaltz/nynjmohonk/R.java
@@ -16,10 +16,12 @@ public final class R {
         public static final int mohonk_map_smaller=0x7f020002;
     }
     public static final class id {
-        public static final int imageView1=0x7f050000;
+        public static final int imageView1=0x7f050001;
+        public static final int launchMap=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int mapview=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
