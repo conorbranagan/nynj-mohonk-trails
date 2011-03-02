61a68686eaa560e2aa8e41e4a39a5a4fcf90850b
conor.branagan@gmail.com <conor.branagan@gmail.com@b6079bb8-4db0-9ca3-396d-66b1b36275a7>
2011-03-02 14:58:54 +0000

    working zoom and move. zoom out now working. beginning override of onDraw method.

--- src/edu/newpaltz/nynjmohonk/MapView.java
+++ src/edu/newpaltz/nynjmohonk/MapView.java
@@ -7,27 +7,28 @@ package edu.newpaltz.nynjmohonk;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.ViewConfiguration;
 import android.widget.ImageView;
 
 public class MapView extends ImageView {
-	public static final float MIN_SCALE = 1f;
-	public static final float MAX_SCALE = 4f;
+	public static final float MIN_SCALE = .2f;
+	public static final float MAX_SCALE = .9f;
 	public static final int GROW = 0;
 	public static final int SHRINK = 1;
 	
-	public boolean isMultiTouch = false;
+	public boolean isMultiTouch = false, doMove = true;
 	private Matrix m;
 	private Bitmap myBitmap;
 	public float prevX = -1, prevY = -1, curX, curY, nextX, nextY, dx, dy;
 	public float distPre = -1, distCur, distMeasure; 
-	public float zoomIn = 1.01f, zoomOut = 0.01f, scale;
-	int mTouchSlop;
+	public float zoomIn = 1.01f, zoomOut = 0.99f, scale;
 	
 	public MapView(Context c, AttributeSet a) {
 		super(c, a);
@@ -38,13 +39,22 @@ public class MapView extends ImageView {
 		Bitmap bit = ((BitmapDrawable)getResources().getDrawable(R.drawable.mohonk_map_smaller)).getBitmap();
         myBitmap = bit.copy(Bitmap.Config.RGB_565, true); // Copy bitmap (so it will be mutable)
         this.setImageBitmap(myBitmap);
-		//mTouchSlop = ViewConfiguration.getTouchSlop();
+        m = getImageMatrix();
+        m.postScale(1, 1);
 	}
 	
     public float getScale(Matrix m) {
 		float[] values = new float[9];
 		this.getImageMatrix().getValues(values);
 		return values[Matrix.MSCALE_X];
+    }
+    
+    @Override
+    public void onDraw(Canvas c) {
+    	super.onDraw(c);
+    	Paint p = new Paint();
+    	p.setColor(Color.RED);
+    	c.drawCircle(10, 10, 10, p);
     }
     
     @Override
@@ -56,7 +66,7 @@ public class MapView extends ImageView {
     		prevY = event.getY(0);
     		return true;
 		}
-		
+				
 		switch(action)
 		{
 			case MotionEvent.ACTION_DOWN:
@@ -64,6 +74,7 @@ public class MapView extends ImageView {
 				curX = event.getX(0);
 				curY = event.getY(0);
 				//invalidate();
+				doMove = true;
 				break;
 			}
 			case MotionEvent.ACTION_POINTER_DOWN:
@@ -75,26 +86,31 @@ public class MapView extends ImageView {
 			case MotionEvent.ACTION_POINTER_UP:
 			{
 				isMultiTouch = false;
+				doMove = false;
 				break;
 			}
 			case MotionEvent.ACTION_MOVE:
 			{
+				
 				// First Finger's coordinates
 	    		curX = event.getX(0);
 	    		curY = event.getY(0);
-				if(!isMultiTouch) {
+				if(!isMultiTouch && doMove) {
+					
 					//translation if only one finger is detected
 					dx = curX - prevX;
 					dy = curY - prevY;
 	    	        
 					m = getImageMatrix();
 	    	        m.postTranslate(dx, dy);
+	    	        
+	    	        
 	    	        setImageMatrix(m);
 	    	        setScaleType(ScaleType.MATRIX);
 	    	        invalidate();
 				} else {
 					// Zoom if two fingers are detected
-					Log.d("DEBUG", "Zoom Touch Event");
+
 					// second Finger's coordinates
 		    		nextX = event.getX(1);
 		    		nextY = event.getY(1);
@@ -104,18 +120,16 @@ public class MapView extends ImageView {
 		    		
 		    		m = getImageMatrix();
 		    		scale = getScale(m);
-			    	if (Math.abs(distMeasure) > mTouchSlop) {
-		    			int mode = distMeasure > 0 ? GROW : (distCur == distPre ? 2 : SHRINK);
-			    		switch (mode) {
-				    		case GROW: // detect fingers reverse pinching
-				    			zoom_In(scale);
-				    		break;
-				    		case SHRINK: // detect fingers pinching
-				    			zoom_Out(scale);
-				    		break;
-			    		}
-			    	}
-			    		
+		    		Log.d("DEBUG", "Distance is: " + distCur);
+	    			int mode = distMeasure > 0 ? GROW : (distCur == distPre ? 2 : SHRINK);
+		    		switch (mode) {
+			    		case GROW: // detect fingers reverse pinching
+			    			zoomIn(scale, distCur);
+			    		break;
+			    		case SHRINK: // detect fingers pinching
+			    			zoomOut(scale, distCur);
+			    		break;
+		    		}
 					distPre = distCur;
 			    }	
 				break;
@@ -127,21 +141,21 @@ public class MapView extends ImageView {
 	   	return true;
 	}
     	
-	public void zoom_In(float scale) {
+	public void zoomIn(float scale, float dist) {
 		if(scale > MAX_SCALE) return; //keeps from zooming in too much
 		
 		m = getImageMatrix();
 		m.postScale(zoomIn, zoomIn, getWidth()/2f, getHeight()/2f);
+				
 		setImageMatrix(m);
         setScaleType(ScaleType.MATRIX);
         invalidate();
 		return;
 	}
 	
-	public void zoom_Out(float scale) {
+	public void zoomOut(float scale, float dist) {
 		if(scale < MIN_SCALE) return; //keeps from zooming out too much
-		
-		m = this.getImageMatrix();
+		m = getImageMatrix();
 		m.postScale(zoomOut, zoomOut, getWidth()/2f, getHeight()/2f);
 		setImageMatrix(m);
         setScaleType(ScaleType.MATRIX);
