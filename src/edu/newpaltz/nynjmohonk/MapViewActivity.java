2815533261b962a7a46caee660d5d842170f2f0c
conor.branagan@gmail.com <conor.branagan@gmail.com@b6079bb8-4db0-9ca3-396d-66b1b36275a7>
2011-03-12 22:25:31 +0000

    added menu with "show current location" and "exit map". setup show current location to center the blue location indicator. added GPS progress indicator to show while searching for a GPS signal. turned on location listener that adjust location indicator as location changes (for purposes of showing functionality). switched back to full size map. adjusted zooming parameters.

--- src/edu/newpaltz/nynjmohonk/MapViewActivity.java
+++ src/edu/newpaltz/nynjmohonk/MapViewActivity.java
@@ -2,9 +2,13 @@ package edu.newpaltz.nynjmohonk;
 
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,14 +16,51 @@ import android.location.LocationManager;
 
 public class MapViewActivity extends Activity {
 	float lastX = -1, lastY = -1;
+	ProgressDialog d = null;
+	MapView m;
 	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_layout);
+        // Show progress dialog until GPS location is found
+        d = ProgressDialog.show(this, "", "Waiting for GPS...");
         // Turn on the location updating
-        //turnOnLocation();
+        turnOnLocation();
+    }
+    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.map_menu, menu);
+    	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	m = (MapView)findViewById(R.id.map_image);
+    	switch(item.getItemId()) {
+    	case R.id.exit_map:
+    		
+    		break;
+    	case R.id.current_location:
+    		if(m == null) {
+
+    		} else {
+    			m.showCurrentLocation();
+    		}
+    		break;
+    	}
+    	return true;
+    }
+    
+    @Override
+    public void onStop() {
+    	super.onStop();
+    	finish();
     }
     
     private void turnOnLocation() {
@@ -29,11 +70,15 @@ public class MapViewActivity extends Activity {
         // Define a location listener and the events that go with it    
         LocationListener locationListener = new LocationListener() {
         	public void onLocationChanged(Location location) {
+        		if(d.isShowing()) {
+        			d.hide();
+        		}
+            	m = (MapView)findViewById(R.id.map_image);
         		double longitude = location.getLongitude();
         		double latitude = location.getLatitude();
         		// In this method we want to update our BitMap to reflect the change in location
-        		Log.d("NYNJ Trails", "LOCATION CHANGED TO: " + longitude + ", " + latitude);
-        		updateMapLocation(longitude, latitude);
+        		Log.d("DEBUG", "LOCATION CHANGED TO: " + longitude + ", " + latitude);
+        		m.updateLocation(longitude, latitude);
         	}
         	
         	public void onStatusChanged(String provider, int status, Bundle extras) {}
