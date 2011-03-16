45485087c3688555fea7fc0da859346ca824d143
deviant205@gmail.com <deviant205@gmail.com@b6079bb8-4db0-9ca3-396d-66b1b36275a7>
2011-03-16 14:25:00 +0000

    updated main menu

--- src/edu/newpaltz/nynjmohonk/MainMenu.java
+++ src/edu/newpaltz/nynjmohonk/MainMenu.java
@@ -8,7 +8,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainMenu extends Activity {
-	Button openMap;
+	Button openMap, closeApp;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +19,12 @@ public class MainMenu extends Activity {
 			public void onClick(View v) {
 				startActivity(new Intent(MainMenu.this, MapViewActivity.class));
 			}
+        });
+        closeApp = (Button) findViewById(R.id.exit);
+        closeApp.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		finish();
+        	}
         });
 	}
 }
