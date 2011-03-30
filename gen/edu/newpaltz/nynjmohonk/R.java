be9af9a1f929279538dc8ceecd0c4b3c02d7954b
deviant205@gmail.com <deviant205@gmail.com@b6079bb8-4db0-9ca3-396d-66b1b36275a7>
2011-03-30 14:55:34 +0000

    Touch ups to splash screen and main menu

--- gen/edu/newpaltz/nynjmohonk/R.java
+++ gen/edu/newpaltz/nynjmohonk/R.java
@@ -11,20 +11,38 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int mohonk_map=0x7f020001;
-        public static final int mohonk_map_smaller=0x7f020002;
+        public static final int hikingphoto=0x7f020000;
+        public static final int ic_current_location=0x7f020001;
+        public static final int ic_exit_map=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int mohonk_map=0x7f020004;
+        public static final int mohonk_map_smaller=0x7f020005;
+        public static final int nynjtc_icon=0x7f020006;
     }
     public static final class id {
-        public static final int imageView1=0x7f050001;
-        public static final int launchMap=0x7f050000;
+        public static final int current_location=0x7f060007;
+        public static final int exit=0x7f060004;
+        public static final int exit_map=0x7f060008;
+        public static final int icon=0x7f060000;
+        public static final int imageView1=0x7f060006;
+        public static final int launchMap=0x7f060002;
+        public static final int map_image=0x7f060005;
+        public static final int menu=0x7f060003;
+        public static final int title=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int mapview=0x7f030001;
+        public static final int map_layout=0x7f030001;
+        public static final int mapview=0x7f030002;
+        public static final int splash=0x7f030003;
+    }
+    public static final class menu {
+        public static final int map_menu=0x7f050000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int current_location=0x7f040002;
+        public static final int exit_map=0x7f040003;
+        public static final int splash_screen=0x7f040001;
     }
 }
