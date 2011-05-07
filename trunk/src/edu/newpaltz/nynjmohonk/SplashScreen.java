860c46abc61ac598839b7e309ea6797a787971ea
deviant205@gmail.com <deviant205@gmail.com@b6079bb8-4db0-9ca3-396d-66b1b36275a7>
2011-05-07 13:08:24 +0000

    Added clickable new paltz logo to splash screen with a toast

--- trunk/src/edu/newpaltz/nynjmohonk/SplashScreen.java
+++ trunk/src/edu/newpaltz/nynjmohonk/SplashScreen.java
@@ -4,11 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class SplashScreen extends Activity {	
 	//protected boolean _active = true, continueOn = true;
 	protected int _splashTime = 5000;
+	Toast t;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,14 @@ public class SplashScreen extends Activity {
 	    		startActivity(new Intent("edu.newpaltz.nynjmohonk.MainMenu"));
 	    	}
 	    }, _splashTime);
+	    
+	    ImageView myImg= (ImageView) findViewById(R.id.npcslogo);
+	    myImg.setOnClickListener(new View.OnClickListener() {
+	                public void onClick(View view) {
+	                	t = Toast.makeText(SplashScreen.this, "Created by Conor Branagan and Jacquelyn Sagaas of the SUNY New Paltz Computer Science Department", Toast.LENGTH_LONG);
+						t.show();
+	                }
+	            });
 	}
 }
 
